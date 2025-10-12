797c335d9edd593ae19b3eae5a065b1833e544f3
Clive Rafiq <cliverafiq@Clives-MacBook-Pro.local>
2025-10-12 00:36:22 -0700

    Initial commit

---
+++ rpg-game/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string playerName;
+    int health = 100;
+    int attack = 15;
+
+    cout << "Enter your player name: ";
+    cin >> playerName;
+    
+}
