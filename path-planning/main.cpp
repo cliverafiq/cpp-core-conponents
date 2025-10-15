37a6c7eb496fc5a21094545acd33759e779757e9
cliverafiq <clive.rafiq@gmail.com>
2025-10-14 22:31:07 -0700

    fea: started a path planning algorithm

---
+++ path-planning/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <limits>
+#include <tuple>
+#include <algorithm>
+
+using namespace std;
+
+struct pos {int x, y;};
+
+int main()
+{
+    vector<string> map = 
+    {
+        "........",
+        ".######.",
+        ".#....#.",
+        ".#.#..#.",
+        ".#.#..#.",
+        ".#....#.",
+        ".######.",
+        "........"
+    };
+
+    pos start{0, 0};
+    pos end{7, 7};
+
+    
+
+    return;
+}
